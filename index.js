f2587a55a8dd76be628dcb12a8fa7c59967afb6a
roy <kimathiwaweru@gmail.com>
2022-11-30 11:09:03 +0300

    assingment completed

--- index.js
+++ index.js
@@ -1,2 +1,59 @@
+// const chai = require('chai');
+// const expect = chai.expect;
+// // Write your code here
+// function multiply(num1, num2) {  
+//      return (num1 * num2) ;
+// }
+// console.log(multiply(31,2));
+// function random(num) {
+//     num = Math.floor(Math.random() * 10) + 1;
+//     if (num > 0)
+//     {
+//         return(num) ;
+//     }else random();     
+// }
+// console.log(random ());
+// function mod(num3, num4) {   
+//   return (num3 % num4);   
+// }
+// console.log (mod(14,5));
+// function maximum (){
+//     nums = [2,3,4,5,6,20,1,14,17,9];
+//     let maxiMum = math.max(nums);
+//     return maxiMum ; } 
+// console.log(maximum());
 
-// Write your code here
+
+// let num1 = 31;
+// let num2 = 2;
+// let num3 = 14;
+// let num4 = 5;
+// let nums = [2,3,4,5,6,20,1,14,17,9];
+// function multiply(num1, num2){
+//     return (num1*num2);
+// }
+// multiply();
+// function random(){
+//     random = math.flor(math.random()*10) +1;
+// }
+// random();
+// function mod (num3,num4){
+//     return (num3 % num4);
+// }
+// mod();
+// function max(){
+//     return (math.max(...nums));
+// }
+// max();
+
+
+let num1=31;
+let num2=2;
+let multiply=num1*num2
+let num3=20
+let num4=5
+let mod=num3/num4
+let max;
+max=Math.max(20,1,3,4)
+ let random;
+ random= (Math.floor(Math.random()*10)+1);
