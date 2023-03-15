2f2868f5244897978688e494e5cfef128452026d
roy <kimathiwaweru@gmail.com>
2023-03-15 13:21:09 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,59 +1,14 @@
-// const chai = require('chai');
-// const expect = chai.expect;
-// // Write your code here
-// function multiply(num1, num2) {  
-//      return (num1 * num2) ;
-// }
-// console.log(multiply(31,2));
-// function random(num) {
-//     num = Math.floor(Math.random() * 10) + 1;
-//     if (num > 0)
-//     {
-//         return(num) ;
-//     }else random();     
-// }
-// console.log(random ());
-// function mod(num3, num4) {   
-//   return (num3 % num4);   
-// }
-// console.log (mod(14,5));
-// function maximum (){
-//     nums = [2,3,4,5,6,20,1,14,17,9];
-//     let maxiMum = math.max(nums);
-//     return maxiMum ; } 
-// console.log(maximum());
+// const multiply = (num1 , num2) => {return (num1 * num2);}
+// multiply(31 ,2);
 
 
-// let num1 = 31;
-// let num2 = 2;
-// let num3 = 14;
-// let num4 = 5;
-// let nums = [2,3,4,5,6,20,1,14,17,9];
-// function multiply(num1, num2){
-//     return (num1*num2);
-// }
-// multiply();
-// function random(){
-//     random = math.flor(math.random()*10) +1;
-// }
-// random();
-// function mod (num3,num4){
-//     return (num3 % num4);
-// }
-// mod();
-// function max(){
-//     return (math.max(...nums));
-// }
-// max();
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+const random = (Math.floor(Math.random()*10)+1);
+const num3 = 24;
+const num4= 5
+const mod=num3 % num4;
+const max = Math.max(20,1,3,4);
 
 
-let num1=31;
-let num2=2;
-let multiply=num1*num2
-let num3=20
-let num4=5
-let mod=num3/num4
-let max;
-max=Math.max(20,1,3,4)
- let random;
- random= (Math.floor(Math.random()*10)+1);
